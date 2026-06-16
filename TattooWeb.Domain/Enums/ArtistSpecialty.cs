be8a403b1b7b64af036af0eecafbf7d858d63fc1
Davi Campaner <campanerdavi@gmail.com>
2026-06-16 16:17:37 -0300

    feat: add ArtistSpecialty enum and update Artist entity and commands

---
+++ TattooWeb.Domain/Enums/ArtistSpecialty.cs
@@ -0,0 +1,15 @@
+﻿namespace TattooWeb.Domain.Enums;
+
+public enum ArtistSpecialty
+{
+    Blackwork,
+    Realismo,
+    Aquarela,
+    Geometrico,
+    OldSchool,
+    Mandala,
+    Fineline,
+    Tribal,
+    Japones,
+    Neotradicional
+}
